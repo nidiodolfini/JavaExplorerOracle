e0e35f35c66170c4dd0c5a22dbb982d9701d61a7
Nidio Dolfini <nidiosdolfini@gmail.com>
2021-02-26 14:26:28 -0300

    Finished exercise lesson Handling Text and Numbers

--- src/duke/choice/Clothing.java
+++ src/duke/choice/Clothing.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package duke.choice;
 
-/**
- *
- * @author nidio
- */
+
+
 public class Clothing {
+    
+    String description;
+    double price;
+    String size = "M";
     
 }
--- src/duke/choice/ShopApp.java
+++ src/duke/choice/ShopApp.java
@@ -15,13 +15,32 @@ public class ShopApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        double tax = 0.2;
+        double total = 0;
         Customer c1 = new Customer();
         c1.name = "Pinky";
         System.out.println("Welcome to Duke Choice Shop " + c1.name);
         
-        String um = "Hello", dois = "World";
-        String messa = um.concat(dois);
-        System.out.println(messa);
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+        
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+        item1.size = "M";
+        
+        item2.description = "Orange T-Shirt";
+        item2.price = 10.5;
+        item2.size = "S";
+        
+        
+        System.out.println("Item one in: " + item1.description + " Price is " + item1.price + " and Size is " + item1.size);
+        System.out.println("Item two in: " + item2.description + " Price is " + item2.price + " and Size is " + item2.size);
+        total = (item1.price + (item2.price * 2));
+        Double totalTax = total * tax;
+        System.out.println(total + totalTax);
+
+        
     }
     
 }
